e40e3cc9277fcc0d0d01c5bd43f69652189f0fc1
Wildney D Fonseca <wildneyfonseca@gmail.com>
2022-01-24 23:11:06 -0300

    Day one css changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
+//Yarn Start: Yarn dev
 //função (argumento1, 2...)
 //${} Valor dinanmico
 //Ctrl/ Comentar código {/* XYZ */}
@@ -75,7 +76,7 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'WILD';
+    const username = 'WILDNEY';
   
     return (
       <>
@@ -83,7 +84,7 @@ export default function PaginaInicial() {
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
-            backgroundColor: appConfig.theme.colors.primary[400],
+            backgroundColor: appConfig.theme.colors.primary[300],
             backgroundImage: 'url(https://i0.wp.com/wallpapercave.com/wp/wp4306669.jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
@@ -100,7 +101,7 @@ export default function PaginaInicial() {
               width: '100%', maxWidth: '700px',
               borderRadius: '5px', padding: '32px', margin: '16px',
               boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-              backgroundColor: appConfig.theme.colors.neutrals[900],
+              backgroundColor: appConfig.theme.colors.novasCores[300],
             }}
           >
             {/* Formulário */}
@@ -111,8 +112,9 @@ export default function PaginaInicial() {
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
               }}
             >
-              <Titulo tag="h2">Boas vindas de volta!</Titulo>
-              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+              <Titulo tag="h2">Welcome back, Rider!</Titulo>
+              <Titulo styleSheet={{color: appConfig.theme.colors.neutrals[900]}}></Titulo>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[100] }}>
                 {appConfig.name}
               </Text>
   
@@ -120,10 +122,10 @@ export default function PaginaInicial() {
                 fullWidth
                 textFieldColors={{
                   neutral: {
-                    textColor: appConfig.theme.colors.neutrals[200],
+                    textColor: appConfig.theme.colors.neutrals[500],
                     mainColor: appConfig.theme.colors.neutrals[900],
                     mainColorHighlight: appConfig.theme.colors.primary[500],
-                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                    backgroundColor: appConfig.theme.colors.novasCores[100],
                   },
                 }}
               />
@@ -133,9 +135,9 @@ export default function PaginaInicial() {
                 fullWidth
                 buttonColors={{
                   contrastColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[500],
-                  mainColorLight: appConfig.theme.colors.primary[400],
-                  mainColorStrong: appConfig.theme.colors.primary[600],
+                  mainColor: appConfig.theme.colors.novasCores[500],
+                  mainColorLight: appConfig.theme.colors.novasCores[900],
+                  mainColorStrong: appConfig.theme.colors.novasCores[700],
                 }}
               />
             </Box>
@@ -163,13 +165,13 @@ export default function PaginaInicial() {
                   borderRadius: '50%',
                   marginBottom: '16px',
                 }}
-                src={`https://github.com/${username}.png`}
+                src={`https://avatars.githubusercontent.com/u/89703756?v=4`}
               />
               <Text
                 variant="body4"
                 styleSheet={{
                   color: appConfig.theme.colors.neutrals[200],
-                  backgroundColor: appConfig.theme.colors.neutrals[900],
+                  backgroundColor: appConfig.theme.colors.neutrals[500],
                   padding: '3px 10px',
                   borderRadius: '1000px'
                 }}
